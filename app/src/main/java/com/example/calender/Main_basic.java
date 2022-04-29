b219ba675fe5d4e5d62116bcf14b3cc12e04f741
jinmin <wlsalsdla1@g.shingu.ac.kr>
2022-04-29 11:00:01 +0900

    메인-기본 하는중

---
+++ app/src/main/java/com/example/calender/Main_basic.java
@@ -0,0 +1,14 @@
+package com.example.calender;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main_basic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_basic);
+    }
+}
