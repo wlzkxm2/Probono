f5ec70ceb61f9f3cf59133622746f768e0e46be9
jinmin <wlsalsdla1@g.shingu.ac.kr>
2022-05-03 23:47:01 +0900

    일정 리스트 애니메이션 하는중

---
+++ app/src/main/java/com/example/calender/Item.java
@@ -0,0 +1,23 @@
+package com.example.calender;
+
+    public class Item {
+        String title;
+        String description;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    }
+
---
+++ app/src/main/java/com/example/calender/Schedule_List_Anim.java
@@ -0,0 +1,85 @@
+package com.example.calender;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Schedule_List_Anim extends RecyclerView.Adapter<Schedule_List_Anim.ViewHolder>{
+
+    ArrayList<Item> items = new ArrayList<Item>();
+
+    int lastPosition = -1;
+
+    Context context;
+
+    static String TAG = "Adapter";
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View itemView = inflater.inflate(R.layout.item_layout, viewGroup, false);
+
+        context = viewGroup.getContext();
+
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+
+        if(viewHolder.getAdapterPosition() > lastPosition){
+
+            //애니메이션 적용
+            Animation animation = AnimationUtils.loadAnimation(context, R.anim.slide_in_row);
+            ((ViewHolder) viewHolder).itemView.startAnimation(animation);
+
+            Item item = items.get(position);
+            viewHolder.setItem(item);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void addItem(Item item){
+        items.add(item);
+    }
+
+    public void removeAllItem(){
+        items.clear();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView title_view;
+        TextView description;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            title_view = itemView.findViewById(R.id.title_text);
+            description = itemView.findViewById(R.id.desc_text);
+        }
+
+        public void setItem(Item item){
+
+            title_view.setText(item.getTitle());
+            description.setText(item.getDescription());
+        }
+    }
+}
