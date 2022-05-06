3c599e474b513c1a603a5849ce47d881e8449c42
jinmin <wlsalsdla1@g.shingu.ac.kr>
2022-05-06 12:04:55 +0900

    메인-기본 리스트 밑 일정추가버튼 애니메이션 추가
    메인-기본 리스트 밑 일정추가버튼 애니메이션 추가

--- app/src/main/java/com/example/calender/Main_Basic.java
+++ app/src/main/java/com/example/calender/Main_Basic.java
@@ -1,16 +1,24 @@
 package com.example.calender;
 
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MotionEventCompat;
 import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,9 +35,17 @@ public class Main_Basic extends FragmentActivity  implements View.OnClickListene
     private Animation floating_open, floating_close;
     private boolean isFabOpen = false;
 
-    TextView now;
-    RecyclerView recyclerView;
+    ImageView add_schedule_dot;
+    ImageButton add_schedule;
+    TextView now, add_schedule_txt;
+    RecyclerView recyclerView, mRecyclerView;
     List_ItemAdapter list_itemAdapter;
+    int listDB=10;
+
+    int lastVisibleItemPositions;
+    int itemTotalCounts;
+
+    Animation fade_in,fade_out;
 
     private String getTime() { //현재 시간 가져오기
         long now = System.currentTimeMillis(); // 현재 시간을 now 변수에 넣음
@@ -39,10 +55,41 @@ public class Main_Basic extends FragmentActivity  implements View.OnClickListene
         return getTime;
     }
 
+//    @Override 테스트용
+//    public boolean onTouchEvent(MotionEvent event) {
+//        int action= MotionEventCompat.getActionMasked(event);
+//        int i=0;
+//        switch (action){
+//            case (MotionEvent.ACTION_UP) :
+//                Log.d("Basic","Up " + i);
+//                i++;
+//                return true;
+//
+//            case (MotionEvent.ACTION_MOVE):
+//                Log.d("Basic","Move " + i);
+//                i++;
+//                return true;
+//
+//            default:
+//                return super.onTouchEvent(event);
+//        }
+//    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_basic);
+
+        fade_in = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.fade_in);
+
+        fade_out = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.fade_out);
+
+        //리스트 밑 일정 등록 버튼
+        add_schedule = (ImageButton) findViewById(R.id.add_schedule);
+        add_schedule_txt = (TextView) findViewById(R.id.add_schedule_txt);
+        add_schedule_dot = (ImageView) findViewById(R.id.add_schedule_dot);
 
         //플로팅 버튼
         mContext = getApplicationContext();
@@ -72,13 +119,11 @@ public class Main_Basic extends FragmentActivity  implements View.OnClickListene
         list_itemAdapter.removeAllItem();
 
         //샘플 데이터 생성
-        for (int i = 0; i < 50; i++) {
-
+        for (int i = 0; i < listDB; i++) {
             List_Item list_item = new List_Item();
             list_item.setTime("14:00" + "-" + i);
             list_item.setTitle("과제하기" + "-" + i);
             list_item.setText("그치만 하기 싫은걸" + "-" + i);
-
             //데이터 등록
             list_itemAdapter.addItem(list_item);
         }
@@ -86,8 +131,33 @@ public class Main_Basic extends FragmentActivity  implements View.OnClickListene
         //적용
         list_itemAdapter.notifyDataSetChanged();
 
-        //애니메이션 실행
+        //리스트 밑 일정 추가 버튼 나타나기
         recyclerView.startLayoutAnimation();
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                lastVisibleItemPositions = ((LinearLayoutManager) recyclerView.getLayoutManager()).findLastCompletelyVisibleItemPosition();
+                itemTotalCounts = recyclerView.getAdapter().getItemCount()-1;
+                if (lastVisibleItemPositions == itemTotalCounts) {
+//                    add_schedule.setVisibility(View.VISIBLE); //화면에 보이게 한다.
+//                    add_schedule_dot.setVisibility(View.VISIBLE); //화면에 보이게 한다.
+//                    add_schedule_txt.setVisibility(View.VISIBLE); //화면에 보이게 한다.
+                    add_schedule.startAnimation(fade_in);
+                    add_schedule_txt.startAnimation(fade_in);
+                    add_schedule_dot.startAnimation(fade_in);
+                } else if (lastVisibleItemPositions != itemTotalCounts){
+                    add_schedule.setVisibility(View.INVISIBLE); //화면에 안보이게 한다.
+                    add_schedule_dot.setVisibility(View.INVISIBLE); //화면에 안보이게 한다.
+                    add_schedule_txt.setVisibility(View.INVISIBLE); //화면에 안보이게 한다.
+//                    add_schedule.startAnimation(fade_out);
+//                    add_schedule_dot.startAnimation(fade_out);
+//                    add_schedule_txt.startAnimation(fade_out);
+
+                }
+            }
+        });
     }
 
     //플로팅버튼 스위치
@@ -126,5 +196,7 @@ public class Main_Basic extends FragmentActivity  implements View.OnClickListene
             isFabOpen = true;
         }
     }
-}
+    }
+
+
 
