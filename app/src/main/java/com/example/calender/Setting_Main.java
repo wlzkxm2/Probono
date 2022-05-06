13a53a42ce0332af232b1a008508b2fe089796d7
IT/소프트 이한민 <glasses0119@g.shingu.ac.kr>
2022-05-06 22:19:43 +0900

    Update Setting_Main.java
    마지함

--- app/src/main/java/com/example/calender/Setting_Main.java
+++ app/src/main/java/com/example/calender/Setting_Main.java
@@ -1,21 +1,43 @@
 package com.example.calender;
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+public class Setting_main extends AppCompatActivity {
+    public static final String TAG="MainActivity";
+    Button mod_btn,mod_btn1;
+    String themeColor;
 
-public class Setting_Main extends Fragment {
-
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.setting_main, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        return view;
+        themeColor=ThemeUtil.modLoad(getApplicationContext());
+        ThemeUtil.applyTheme(themeColor);
+        //버튼 찾아주기
+        mod_btn=findViewById(R.id.light_btn);
+        mod_btn1=findViewById(R.id.dark_btn);
+        //라이트모드 버튼을 눌렀을대
+        mod_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                themeColor =ThemeUtil.LIGHT_MODE;
+                ThemeUtil.applyTheme(themeColor);
+                ThemeUtil.modSave(getApplicationContext(),themeColor);
+            }
+        });
+        //다크모드 버튼을 눌렀을때
+        mod_btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                themeColor =ThemeUtil.DARK_MODE;
+                ThemeUtil.applyTheme(themeColor);
+                ThemeUtil.modSave(getApplicationContext(),themeColor);
+            }
+        });
     }
 }
