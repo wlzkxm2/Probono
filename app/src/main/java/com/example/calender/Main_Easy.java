69aef164ed30c29063b0721f463dc259e5aa460c
jinmin <wlsalsdla1@g.shingu.ac.kr>
2022-05-11 15:13:51 +0900

    메인-이지 만드는중이다 맨이야
    메인-이지 만드는중이다 맨이야

--- app/src/main/java/com/example/calender/Main_Easy.java
+++ app/src/main/java/com/example/calender/Main_Easy.java
@@ -1,4 +1,59 @@
 package com.example.calender;
 
-public class Main_Easy {
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main_Easy extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    int listDB = 10;
+    private List_ItemAdapter_Easy list_itemAdapter_easy;
+    TextView now;
+
+    private String getTime() { //현재 시간 가져오기
+        long now = System.currentTimeMillis(); // 현재 시간을 now 변수에 넣음
+        Date date = new Date(now); // 현재 시간을 date 형식으로 변환
+        SimpleDateFormat dateFormat = new SimpleDateFormat("h시 m분");
+        String getTime = dateFormat.format(date);
+        return getTime;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_easy);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view_easy);
+        list_itemAdapter_easy = new List_ItemAdapter_Easy();
+        recyclerView.setAdapter(list_itemAdapter_easy);
+
+        now = (TextView) findViewById(R.id.main_easy_now);
+        now.setText(getTime());
+
+        //화면 클리어
+        list_itemAdapter_easy.removeAllItem();
+
+        //샘플 데이터 생성
+        for (int i = 0; i < listDB; i++) {
+            List_Item list_item = new List_Item();
+            list_item.setTime("14:00" + "-" + i);
+            list_item.setTitle("과제하기" + "-" + i);
+            //데이터 등록
+            list_itemAdapter_easy.addItem(list_item);
+        }
+
+        //적용
+        list_itemAdapter_easy.notifyDataSetChanged();
+        
+
+    }
+
 }
