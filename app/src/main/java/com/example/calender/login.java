414d71d46b1ba836c18c711deb1961c0330539dd
IT/소프트 이한민 <glasses0119@g.shingu.ac.kr>
2022-06-02 12:40:13 +0900

    로그인

--- app/src/main/java/com/example/calender/login.java
+++ app/src/main/java/com/example/calender/login.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class login extends AppCompatActivity {
     Button loginbtn,registerbtn;
+    EditText idkan,passkan;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +19,8 @@ public class login extends AppCompatActivity {
         //알림 설정 레이아웃을 표시해준다
         loginbtn = (Button) findViewById(R.id.login_btn);
         registerbtn = (Button) findViewById(R.id.register_btn);
+        idkan=(EditText)findViewById(R.id.emailkan);
+        passkan=(EditText)findViewById(R.id.password_kan);
         loginbtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
