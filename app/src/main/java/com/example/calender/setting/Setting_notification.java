1804ac43a53dde4837a8d1afe9b0b6330a413753
IT/소프트 이한민 <glasses0119@g.shingu.ac.kr>
2022-06-16 21:05:06 +0900

    스위치 클래스 등록 상단바 색 수정

--- app/src/main/java/com/example/calender/setting/Setting_notification.java
+++ app/src/main/java/com/example/calender/setting/Setting_notification.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.Switch;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,7 @@ import com.example.calender.R;
 public class Setting_notification extends AppCompatActivity {
     Button back;
     View.OnClickListener cl;
+    Switch notisw;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,6 +25,17 @@ public class Setting_notification extends AppCompatActivity {
         //알림 설정 레이아웃을 표시
 
         back = (Button) findViewById(R.id.back_noti);
+        notisw=(Switch) findViewById(R.id.noti_switch) ;
+        (notisw).setChecked(true);
+        (notisw).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    //True이면 할 일
+                }else{
+                    //False이면 할 일
+                }
+            }
+        });
 
 
         cl = new View.OnClickListener() {  //뒤로가기 버튼 누르면 화면 종료되도록 설정
