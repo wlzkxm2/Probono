ee11cc33d73eaf97fde3a197ac55e18431fd9fea
IT/소프트 이한민 <glasses0119@g.shingu.ac.kr>
2022-10-18 15:13:07 +0900

    유저프로필 로그아웃 버튼 수정 리스트 일정 색 다크모드에 맞춰 수정

--- app/src/main/java/com/example/calender/nav1.java
+++ app/src/main/java/com/example/calender/nav1.java
@@ -3,6 +3,7 @@ package com.example.calender;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
@@ -34,10 +35,35 @@ public class nav1 extends AppCompatActivity {
     private Setting_main setting_main;
     String themeColor;
     private Permission permission;
-
+    // 마지막으로 뒤로가기 버튼을 눌렀던 시간 저장
+    private long backKeyPressedTime = 0;
+    // 첫 번째 뒤로가기 버튼을 누를때 표시
+    private Toast toast;
     @Override
     public void onBackPressed() {
-        //super.onBackPressed();
+        // 기존 뒤로가기 버튼의 기능을 막기위해 주석처리 또는 삭제
+        // super.onBackPressed();
+
+        // 마지막으로 뒤로가기 버튼을 눌렀던 시간에 2초를 더해 현재시간과 비교 후
+        // 마지막으로 뒤로가기 버튼을 눌렀던 시간이 2초가 지났으면 Toast Show
+        // 2000 milliseconds = 2 seconds
+        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            backKeyPressedTime = System.currentTimeMillis();
+            toast = Toast.makeText(this, "\'뒤로\' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+        // 마지막으로 뒤로가기 버튼을 눌렀던 시간에 2초를 더해 현재시간과 비교 후
+        // 마지막으로 뒤로가기 버튼을 눌렀던 시간이 2초가 지나지 않았으면 종료
+        // 현재 표시된 Toast 취소
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+            finish();
+            moveTaskToBack(true); // 태스크를 백그라운드로 이동
+            finishAndRemoveTask(); // 액티비티 종료 + 태스크 리스트에서 지우기
+
+            System.exit(0);
+            toast.cancel();
+        }
     }
 
     @Override
@@ -73,10 +99,10 @@ public class nav1 extends AppCompatActivity {
                         setFrag(1);
 
 //                        // 네비게이션 버튼 똑같은거 눌렀을 때 새로고침
-//                        overridePendingTransition(0, 0);//인텐트 효과 없애기
-//                        Intent intent = getIntent(); //인텐트
-//                        startActivity(intent); //액티비티 열기
-//                        overridePendingTransition(0, 0);//인텐트 효과 없애기
+//                        overridePendingTransition(1, 1);//인텐트 효과 없애기
+//                        Intent j = getIntent(); //인텐트
+//                        startActivity(j); //액티비티 열기
+//                        overridePendingTransition(1, 1);//인텐트 효과 없애기
 
                         break;
                     case R.id.navi_setting:
